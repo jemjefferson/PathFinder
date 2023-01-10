81a4ff7cb8ae036519a72a5540b246b9f514e400
Jordan Jefferson <jemjefferson@gmail.com>
2023-01-10 16:56:55 -0600

    Restructured html and small styling update.

--- src/HTML/index.jsx
+++ src/HTML/index.jsx
@@ -11,8 +11,19 @@ export default class Index extends Component {
     }
 render() {
     return (
+      <div>
       <div id="header">
         <h1>Path Finder</h1>
+      </div>
+      <div id="tutorialText">
+      <p id="tutorial">Welcome to Path Finder. This application (in development) finds the shortest path<br></br> from a starting position to a finishing position using Dijkstra's algorithm.<br></br>
+      Click and drag to create walls and barriers between the two positions. <br></br>Click the button in order to find the shortest path between the two positions.</p>
+      </div>
+      <div id="logos">
+        <div>
+          
+        </div>
+      </div>
       </div>
     );
 }}
